3432abe9b0e14daa048bdee2706133c7079e550f
alex-msk <alex@alexmsk.com>
2020-03-04 19:29:25 +0300

    Add first and last name to contact BaseModel

--- amocrm/apimodels.py
+++ amocrm/apimodels.py
@@ -312,6 +312,8 @@ class BaseLead(_BaseModel):
 
 
 class BaseContact(_AbstractNamedModel):
+    first_name = fields._Field('first_name')
+    last_name = fields._Field('last_name')           
     leads_model = None
     leads = fields.ManyForeignField(BaseLead, 'linked_leads_id')
     type = fields._ConstantField('type', 'contact')
